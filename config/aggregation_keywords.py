113c7209d4e34149820cf29843ac645488fb15da
practise30 <practise30@gmail.com>
2026-05-08 15:23:30 +0900

    feat: Refactor prompt architecture

---
+++ config/aggregation_keywords.py
@@ -0,0 +1,7 @@
+AGGREGATION_KEYWORDS = [
+
+    "평균",
+    "합계",
+    "총합",
+    "개수"
+]
--- config/target_map.py
+++
@@ -1,9 +0,0 @@
-from util.flatten import flatten_map
-
-TARGET_MAP = {
-    "inventory": [
-        "상품", "상품들", "제품", "제품들", "물건", "물건들"
-        ]
-}
-
-ALL_TARGET_KEYWORDS = flatten_map(TARGET_MAP)
